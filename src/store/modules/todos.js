1ec39e9660ef0faca4c4cb94132d6b1f147cee7d
Bruno Gonzales <brunogonzales1129@gmail.com>
2020-04-01 12:19:17 -0500

    Initial commit

---
+++ src/store/modules/todos.js
@@ -0,0 +1,69 @@
+import axios from "axios";
+
+const state = {
+  todos: []
+};
+
+const getters = {
+  allTodos: state => state.todos
+};
+
+const actions = {
+  async fetchTodos({ commit }) {
+    const response = await axios.get(
+      "https://jsonplaceholder.typicode.com/todos"
+    );
+    commit("setTodos", response.data);
+  },
+
+  async addTodo({ commit }, title) {
+    const response = await axios.post(
+      "https://jsonplaceholder.typicode.com/todos",
+      {
+        title,
+        completed: false
+      }
+    );
+    commit("newTodo", response.data);
+  },
+
+  async deleteTodo({ commit }, id) {
+    await axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`);
+    commit("removeTodo", id);
+  },
+
+  async filterTodos({ commit }, limit) {
+    const response = await axios.get(
+      `https://jsonplaceholder.typicode.com/todos?_limit=${limit}`
+    );
+    commit("setTodos", response.data);
+  },
+
+  async updateTodo({ commit }, updatedTodo) {
+    const response = await axios.put(
+      `https://jsonplaceholder.typicode.com/todos/${updatedTodo.id}`,
+      updatedTodo
+    );
+    commit("updateTodo", response.data);
+  }
+};
+
+const mutations = {
+  setTodos: (state, todos) => (state.todos = todos),
+  newTodo: (state, newTodo) => (state.todos = [newTodo, ...state.todos]),
+  removeTodo: (state, id) =>
+    (state.todos = state.todos.filter(todo => todo.id !== id)),
+  updateTodo: (state, updatedTodo) => {
+    const todoIndex = state.todos.findIndex(todo => todo.id === updatedTodo.id);
+    if (todoIndex !== -1) {
+      state.todos.splice(todoIndex, 1, updatedTodo);
+    }
+  }
+};
+
+export default {
+  state,
+  getters,
+  actions,
+  mutations
+};
